0101712d8837db9a0d4ff7f69639a67c220e5527
Tigran <tigrangrigoryan.it@gmail.com>
2021-03-26 12:29:37 -0800

    corrected two errors

--- app/src/Components/Main/footer-about/FooterAbout.jsx
+++ app/src/Components/Main/footer-about/FooterAbout.jsx
@@ -10,31 +10,31 @@ const FooterAbout = () => {
         <div className={'footer-about'}>
             <div className={'footer-about__box'}>
                 <Button className={'footer-about__box__btn'}><img className={'footer-about__box__btn__img'} src={busIcon}/></Button>
-                <dov className={'footer-about__box__info'}>
+                <div className={'footer-about__box__info'}>
                     <p className={'footer-about__box__info__title'}>Արագ Առաքում</p>
                     <p className={'footer-about__box__info__text'}>Արագ եւ արդյունավետ առաքում</p>
-                </dov>
+                </div>
             </div>
             <div className={'footer-about__box'}>
                 <Button className={'footer-about__box__btn'}><img className={'footer-about__box__btn__img'} src={callMy}/></Button>
-                <dov className={'footer-about__box__info'}>
+                <div className={'footer-about__box__info'}>
                     <p className={'footer-about__box__info__title'}>Զանգահարեք Մեզ</p>
                     <p className={'footer-about__box__info__text'}>Միշտ սպասում ենք Ձեր զանգին</p>
-                </dov>
+                </div>
             </div>
             <div className={'footer-about__box'}>
                 <Button className={'footer-about__box__btn'}><img className={'footer-about__box__btn__img'} src={loveYou}/></Button>
-                <dov className={'footer-about__box__info'}>
+                <div className={'footer-about__box__info'}>
                     <p className={'footer-about__box__info__title'}>Բարձրակարգ Սպասարկում</p>
                     <p className={'footer-about__box__info__text'}>Սիրում ենք մեր հաճախորդներին</p>
-                </dov>
+                </div>
             </div>
             <div className={'footer-about__box'}>
                 <Button className={'footer-about__box__btn'}><img className={'footer-about__box__btn__img'} src={present}/></Button>
-                <dov className={'footer-about__box__info'}>
+                <div className={'footer-about__box__info'}>
                     <p className={'footer-about__box__info__title'}>Որակյալ Նվերներ</p>
                     <p className={'footer-about__box__info__text'}>Միայն որակյան նվերներ</p>
-                </dov>
+                </div>
             </div>
 
         </div>
